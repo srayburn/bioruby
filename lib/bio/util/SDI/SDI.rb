7a57d3c785d7948f8c646f05c143df2c03b587fe
Sara Rayburn <sarara@sarara.local>
2010-06-07 12:12:04 -0500

    mapping implemented, working

--- lib/bio/util/SDI/SDI.rb
+++ lib/bio/util/SDI/SDI.rb
@@ -13,18 +13,25 @@ require 'bio/db/phyloxml/phyloxml_parser'
 class SDI
 
   def initialize
+    # load phyloxml trees (assumption: each file contains only 1 tree)
     @gene_tree = Bio::PhyloXML::Parser.open('G_test.xml')
     @species_tree = Bio::PhyloXML::Parser.open('S_test.xml')
     @gene_tree = @gene_tree.next_tree
     @species_tree = @species_tree.next_tree
-    #test rooted
-    #need to make exception, with boolean returning functions
+    
+    # test rooted
+    # need to make exception, with boolean returning functions
     isRooted(@gene_tree)
     isRooted(@species_tree)
-    #test subset
-    #need to make exception, with boolean returning functions
+    
+    # test subset
+    # need to make exception, with boolean returning functions
     isSubset(@species_tree, @gene_tree)
-    #map
+    
+    # map
+    @map = {}
+    @map = initializeMapping(@species_tree, @gene_tree)
+    puts @map
   end #initialize
 
   def isRooted(tree)
@@ -51,25 +58,50 @@ class SDI
     }
     end #isSubset
 
-  def preOrder(root, map)
+  def preOrder(tree)
+    root = tree.root
     index = 1
-    map[index] = root.taxonomies[0].taxonomy_id.value
-      index += 1
-      nextNodes = children(root)
-      nextNodes.each { |n|
-        index = _preOrder(n, index, map)
-      }
-  end
-  def _preOrder(node, index, map)
-    map[index] = node.taxonomies[0].taxonomy_id.value
+    map = {}
+    map['root'] = index
     index += 1
-    nextNodes = children(node)
+    nextNodes = tree.children(root)
+    nextNodes.each { |n|
+      index = _preOrder(tree, n, index, map)
+    }
+   
+    return map
+  end
+ 
+  def _preOrder(tree, node, index, map)
+    puts node.taxonomies
+    if node.taxonomies[0]:
+      map[node.taxonomies[0].taxonomy_id.value] = index
+    else
+      map['internal'] = index
+    end #if
+    index += 1
+   # puts node
+    nextNodes = tree.children(node)
+  #  puts nextNodes
     nextNodes.each{ |n|
-      index = _preOrder(n, index, map)
+      index = _preOrder(tree, n, index, map)
     }
     return index
   end
+
+  def initializeMapping(spec_tree, gene_tree)
+    species_numbering = {}
+    species_numbering = preOrder(spec_tree)
+    puts species_numbering
+    map = {}
+    gene_tree.leaves.each{ |n|
+      map[n.taxonomies[0].taxonomy_id.value] = species_numbering[n.taxonomies[0].taxonomy_id.value]
+    }
+    return map
+  end
+  
   private :_preOrder
+
 end
 
 sdi = SDI.new
