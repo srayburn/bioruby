679a4bf1ad81262b3985a0f90acd8569ccab2991
Sara Rayburn <sarara@sarara.local>
2010-06-07 10:53:04 -0500

    reorganized, fixed require problems, ironed out compile errors

--- lib/bio/util/SDI/SDI.rb
+++ lib/bio/util/SDI/SDI.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 # = util/SDI/sdi_binary.rb - Binary SDI
 #
 # Copyright::   Copyright (C) 2010
@@ -7,62 +6,71 @@
 #
 
 #  load libraries required
-$: << '/Users/sarara/bioruby/lib'
+#$: << '/Users/sarara/bioruby/lib'
 require 'bio/db/phyloxml/phyloxml_parser'
 
-module Bio
-  module SDI
 
-  #  def load
-      @G = Bio::PhyloXML::Parser.open('G_test.xml')
-      @S = Bio::PhyloXML::Parser.open('S_test.xml')
-   # end
+class SDI
 
-    #def isRooted
-    @S_tree = @S.next_tree
-    if @S_tree.root == nil:
-	exit
-    end
-    @G_tree = @G.next_tree
-    if @G_tree.root == nil:
-        exit
-    end
-   # end
+  def initialize
+    @gene_tree = Bio::PhyloXML::Parser.open('G_test.xml')
+    @species_tree = Bio::PhyloXML::Parser.open('S_test.xml')
+    @gene_tree = @gene_tree.next_tree
+    @species_tree = @species_tree.next_tree
+    #test rooted
+    #need to make exception, with boolean returning functions
+    isRooted(@gene_tree)
+    isRooted(@species_tree)
+    #test subset
+    #need to make exception, with boolean returning functions
+    isSubset(@species_tree, @gene_tree)
+    #map
+  end #initialize
 
-   # def isSubset
-      @S_tree = @S.next_tree
-      S_leaves = @S_tree.leaves
-      species = []
-      S_leaves.each { |node|
-       species[species.length] = node.taxonomies[0].taxonomy_id.value
-      }
+  def isRooted(tree)
+    if tree.root == nil:
+      exit
+    end #if
+    
+  end #isRooted
+ 
+  def isSubset(superset, subset)
+    superset_leaves = superset.leaves
+    species = []
+    superset_leaves.each { |node|
+      species[species.length] = node.taxonomies[0].taxonomy_id.value
+    }
 
-
-      @G_tree = @G.next_tree
-      G_leaves = @G_tree.leaves
-      G_leaves.each { |node|
-	key = node.taxonomies[0].taxonomy_id.value
-	puts species.find { |spec| spec == key }
+    subset_leaves = subset.leaves
+    subset_leaves.each { |node|
+      key = node.taxonomies[0].taxonomy_id.value
+      puts species.find { |spec| spec == key }
 	#if node.taxonomies[0].taxonomy_id.value in species:
 	#	puts "ok"
 	#end
-      }
+    }
+    end #isSubset
 
-    def preOrder(root, map)
-      index = 1
-      map[index++] = root.taxonomies[0].taxonomy_id.value
+  def preOrder(root, map)
+    index = 1
+    map[index] = root.taxonomies[0].taxonomy_id.value
+      index += 1
       nextNodes = children(root)
       nextNodes.each { |n|
         index = _preOrder(n, index, map)
-    end
-    def _preOrder(node, index, map)
-      map[index++] = node.taxonomies[0].taxonomy_id.value
-      nextNodes = children(node)
-      nextNodes.each{ |n|
-        index = _preOrder(n, index, map)
-      return index
-    end
-    private :_preOrder(node)
-    end
+      }
+  end
+  def _preOrder(node, index, map)
+    map[index] = node.taxonomies[0].taxonomy_id.value
+    index += 1
+    nextNodes = children(node)
+    nextNodes.each{ |n|
+      index = _preOrder(n, index, map)
+    }
+    return index
+  end
+  private :_preOrder
 end
-end
+
+sdi = SDI.new
+
